90bbafaaec520772dfd451b84ff44c2461b60991
maicaosgz <malconaugusto@outlook.com>
2021-09-15 22:28:14 -0300

    aula variaveis - js iniciantes

---
+++ js-iniciantes/variaveis/aula/script.js
@@ -0,0 +1,16 @@
+var nome = 'malcon';
+var idade = 28;
+var possuiFacul = false;
+
+console.log(nome, idade, possuiFacul);
+
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var semDefinir;
+
+console.log(semDefinir);
---
+++ js-iniciantes/variaveis/exercicio/script.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+const nome = 'Malcon'
+// Declarar uma variável com a sua idade
+let idade = 18;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comida;
+// Atribuir valor a sua comida favorita
+comida = 'Churrasco'
+// Declarar 5 variáveis diferentes sem valores
+let dinheiro;
+let luta;
+let dia;
+let familia;
+let emprego;
+
+console.log(nome,idade,comida,dinheiro,luta,dinheiro,familia,emprego);
