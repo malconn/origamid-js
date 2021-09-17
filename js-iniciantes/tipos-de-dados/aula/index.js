030913e5abe5024e8a477d8f05a1049eca7d4ee5
maicaosgz <malconaugusto@outlook.com>
2021-09-16 22:03:24 -0300

    aula tipos de dados - js iniciantes

---
+++ js-iniciantes/tipos-de-dados/aula/index.js
@@ -0,0 +1,12 @@
+var nome = 'malcon'
+var idade = 18;
+var time;
+console.log(typeof time);
+
+
+var sobrenome = 'Augusto';
+
+var nomeCompleto = nome + ' '+ sobrenome
+
+console.log(nomeCompleto);
+
---
+++ js-iniciantes/tipos-de-dados/exercicio/index.js
@@ -0,0 +1,22 @@
+// Declare uma variável contendo uma string
+let str = 'string';
+// Declare uma variável contendo um número dentro de uma string
+let temNumero = 'tem o número 28';
+// Declare uma variável com a sua idade
+let idade = 18;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nome = 'Malcon';
+let sobrenome = 'Augusto';
+let nomeCompleto = `${nome} ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+let frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+let seeTypeof = typeof nome;
+
+console.log('str:', str);
+console.log('temNumero:', temNumero);
+console.log('idade:', idade);
+console.log('nome Completo:', nomeCompleto);
+console.log('frase:', frase);
+console.log('Typeof:', seeTypeof);
