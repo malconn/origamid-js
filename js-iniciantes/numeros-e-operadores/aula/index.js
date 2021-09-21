3545a8640ba7d6cd7748eaa51e772f72f7199175
maicaosgz <malconaugusto@outlook.com>
2021-09-20 22:26:37 -0300

    aula numero e operadores-01 - js-iniciantes

---
+++ js-iniciantes/numeros-e-operadores/aula/index.js
@@ -0,0 +1,12 @@
+let idade = 2e1;
+let total = 10 + 5 + 10;
+let divisao = 200 / 5;
+let modulo = 514654464 % 3;
+
+let testeNaN = 'Isso é 100' / 2;
+
+console.log(idade);
+console.log(total);
+console.log(divisao);
+console.log(modulo);
+console.log(isNaN(testeNaN));
