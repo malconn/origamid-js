34a11c601d5891b906b615869da8a9d98e3a50d7
malconn <malconaugusto@outlook.com>
2021-11-04 13:15:14 -0300

    tudo-e-objeto (exercicio correção.) -  js-iniciantes

--- js-iniciantes/tudo-e-objeto/exercicio/index.js
+++ js-iniciantes/tudo-e-objeto/exercicio/index.js
@@ -1,17 +1,17 @@
 // nomeie 3 propriedades ou métodos de strings
 const stringName = 'Malcon';
-stringName.toLocaleLowerCase()
-stringName.toLocaleUpperCase()
-stringName.length
+// stringName.toLocaleLowerCase()
+// stringName.toLocaleUpperCase()
+// stringName.length
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
 const testeDom =  document.querySelector('.teste-btn');
 
-const testeDomontoggle = testeDom.ontoggle;
-const testeDomonsubmit = testeDom.onsubmit;
-const testeDomondragover = testeDom.ondragover;
-const testeDomondragstart = testeDom.ondragstart
-const testeDominnerText =  testeDom.innerText;
+// const testeDomontoggle = testeDom.ontoggle;
+// const testeDomonsubmit = testeDom.onsubmit;
+// const testeDomondragover = testeDom.ondragover;
+// const testeDomondragstart = testeDom.ondragstart
+// const testeDominnerText =  testeDom.innerText;
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
