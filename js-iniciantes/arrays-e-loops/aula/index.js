9f0e955086e210f6b71a8219f39a86ffbb1bee7f
malconn <malconaugusto@outlook.com>
2021-11-05 15:28:27 -0300

    arrays-e-loops (aula feita.) - js-iniciantes

---
+++ js-iniciantes/arrays-e-loops/aula/index.js
@@ -0,0 +1,15 @@
+const videoGames = ['switch', 'ps4', 'xbox'];
+
+const ultimoItem = videoGames.pop();
+
+
+for(let numero = 0; numero <= 10; numero++){
+  console.log(numero)
+}
+
+i = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
