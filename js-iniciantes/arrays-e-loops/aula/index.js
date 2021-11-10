9632c8cfa7eedafd12267716b9a10f3f424c02fb
maicaosgz <malconaugusto@outlook.com>
2021-11-09 22:35:43 -0300

    arrays-e-loops (aula feita.) - js-iniciantes

--- js-iniciantes/arrays-e-loops/aula/index.js
+++ js-iniciantes/arrays-e-loops/aula/index.js
@@ -1,23 +1,24 @@
-const videoGames = ['switch', 'ps4', 'xbox', '3DS'];
+const videoGames = ["switch", "ps4", "xbox", "3DS"];
 
-
-
-for(let numero = 1; numero <= 4; numero++){
-  console.log(numero)
+for (let numero = 1; numero <= 4; numero++) {
+  console.log(numero);
 }
 
 let i = 0;
 
 while (i <= 10) {
   console.log(i);
-  i= i+5;
+  i = i + 5;
 }
 
-for(let item =0; item < videoGames.length; item ++){
-  if(videoGames[item] === 'ps4'){
+for (let item = 0; item < videoGames.length; item++) {
+  if (videoGames[item] === "ps4") {
     break;
   }
-  console.log(videoGames[item])
-
+  console.log(videoGames[item]);
 }
+let frutas = ["Banana", "Pera", "Maçã", "Abacaxi", "Uva"];
 
+frutas.forEach(function (fruta, index, array) {
+  console.log(fruta, index, array);
+});
