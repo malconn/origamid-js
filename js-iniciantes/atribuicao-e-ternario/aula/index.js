a19e92e90238c73679ecc15006cadd8fd98d3720
malconn <malconaugusto@outlook.com>
2021-11-11 12:26:16 -0300

    atribuicao-e-ternario (aula feita.) - js-iniciantes

---
+++ js-iniciantes/atribuicao-e-ternario/aula/index.js
@@ -0,0 +1,23 @@
+let numero  = 20;
+let numero2 = 10;
+
+numero += numero2;
+
+console.log(numero);
+
+
+let idade  = 19;
+let naoPossuiDiabetes = true;
+
+let podeBeber;
+
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'PODE BEBER' : 'NÃO PODE BEBER';
+
+console.log(podeBeber)
+
+let possuiFaculdade = true;
+
+if(possuiFaculdade)
+  console.log('Sim possui');
+else
+  console.log('Isso mesmo');
