6eccd75f9a26a2322cd530e180f3acb5e2f2374c
malconn <malconaugusto@outlook.com>
2021-11-12 18:53:18 -0300

    escopo (correcao exercicio) - js-iniciantes

--- js-iniciantes/escopo/exercicio/index.js
+++ js-iniciantes/escopo/exercicio/index.js
@@ -21,11 +21,11 @@ console.log(somarDois(4));
 console.log(dividirDois(6));
 
 // O que fazer para total retornar 500?
-// var numero = 50;
+const numero = 50;
 
-for(let numero = 0; numero <= 50; numero++) {
-  const total = 10 * numero;
-  console.log(total);
+for(let numero = 0; numero <= 10; numero++) {
+  console.log(numero);
 }
 
-// console.log(total);
+const total = 10 * numero;
+console.log(total);
