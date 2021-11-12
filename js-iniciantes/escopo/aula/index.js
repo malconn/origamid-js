57bc394b1e6e0d78b02be7070ed27921e49e6e15
malconn <malconaugusto@outlook.com>
2021-11-12 08:28:39 -0300

    escopo (aula feita.) - js-iniciantes

---
+++ js-iniciantes/escopo/aula/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+function mostrarCarro(){
+  
+  var carro = 'Fusca';
+  console.log(carro)
+}
+
+mostrarCarro();
+
+
+{
+  let mes = 'Dezembro';
+
+  console.log(mes)
+}
+
+// console.log(mes)
+
+// for(let i = 0; i < 10; i++){
+//   console.log(`Número ${i}`);
+// }
+
+// console.log(i)
+
+
+const semana = 'Sexta';
+// semana = 'Quinta';
+
+console.log(semana)
+
+
+const data = {
+  ano: 2021,
+  mes:'Dezembro',
+}
+
+data.ano = 2022;
+
+data.dia = 5;
