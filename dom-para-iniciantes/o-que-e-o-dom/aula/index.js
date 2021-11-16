6ead8bf3dd076d68b73a535e9f26d78e54bae8ab
malconn <malconaugusto@outlook.com>
2021-11-16 12:19:20 -0300

    Iniciando novo módulo & o-que-e-o-dom (aula feita.) - dom-para-iniciantes

---
+++ dom-para-iniciantes/o-que-e-o-dom/aula/index.js
@@ -0,0 +1,10 @@
+
+const href = window.location.href;
+
+console.log(href)
+
+if(href === 'http://127.0.0.1:5500/aula/index.html'){
+  console.log('é igual')
+}
+
+const h1Selecionado =  document.querySelector('h1');
