915b141c746496730ebe12620a0246e34805408c
malconn <malconaugusto@outlook.com>
2021-11-17 12:43:29 -0300

    o-que-e-o-dom (exercicio feito.) - dom-para-iniciantes

--- dom-para-iniciantes/o-que-e-o-dom/aula/index.js
+++ dom-para-iniciantes/o-que-e-o-dom/aula/index.js
@@ -8,3 +8,9 @@ if(href === 'http://127.0.0.1:5500/aula/index.html'){
 }
 
 const h1Selecionado =  document.querySelector('h1');
+
+const h1Classes = h1Selecionado.classList;
+
+h1Selecionado.addEventListener('click', ()=>{
+  console.log('clicou em :', h1Selecionado.innerText);
+})
---
+++ dom-para-iniciantes/o-que-e-o-dom/exercicio/index.js
@@ -0,0 +1,19 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlActual = window.location.href
+
+console.log(urlActual);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo');
+
+console.log(ativo);
+
+// Retorne a linguagem do navegador
+const language = navigator.language;
+console.log(language);
+
+// Retorne a largura da janela
+
+const largura = window.outerWidth;
+
+console.log(largura)
