33b60ace7c4bd8d6826c66c115e8b862bdd8738c
maicaosgz <malconaugusto@outlook.com>
2021-11-23 23:13:40 -0300

    selecao-de-elementos (aula feita.) - dom-para-iniciantes

---
+++ dom-para-iniciantes/selecionar-o-dom/aula/index.js
@@ -0,0 +1,18 @@
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+console.log(gridSection[0]);
+
+const primeiraLi = document.querySelector("li");
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector("ul");
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
+
+const imgAnimais = document.querySelectorAll(".animais img");
+console.log(imgAnimais);
