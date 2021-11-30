951210d41a911d63b520e095ff98f5c5c2471b2e
malconn <malconaugusto@outlook.com>
2021-11-30 15:34:28 -0300

    selecao-de-elementos (coreecao exercicio) - dom-para-iniciantes

--- dom-para-iniciantes/selecionar-o-dom/exercicio/index.js
+++ dom-para-iniciantes/selecionar-o-dom/exercicio/index.js
@@ -1,24 +1,18 @@
 // Retorne no console todas as imagens do site
 const allImgs = document.querySelectorAll("img");
-allImgs.forEach((item) => {
-  console.log(item);
-});
+console.log(allImgs);
 // Retorne no console apenas as imagens que começaram com a palavra imagem
 const allImgsName = document.querySelectorAll('img[src^="img/imagem"]');
 
-allImgsName.forEach((item) => {
-  console.log(item);
-});
+console.log(allImgsName)
 // Selecione todos os links internos (onde o href começa com #)
 const linksInternos = document.querySelectorAll('[href^="#"]');
 
-linksInternos.forEach((item) => {
-  console.log(item);
-});
+console.log(linksInternos);
 // Selecione o primeiro h2 dentro de .animais-descricao
 const primeiroh2 = document.querySelector(".animais-descricao h2");
 console.log(primeiroh2);
 
 // Selecione o último p do site
-const ultimoP = document.querySelector("p").lastChild;
-console.log(ultimoP);
+const ultimoP = document.querySelectorAll("p")
+console.log(ultimoP[ultimoP.length -1]);
