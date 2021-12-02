97d62fb46ac5348cb0c04731db024e174a07f227
malconn <malconaugusto@outlook.com>
2021-12-02 13:24:51 -0300

    foreach-e-arrowfunction (aula feita.) - dom-para-iniciantes

---
+++ dom-para-iniciantes/foreach-e-arrowfunction/aula/index.js
@@ -0,0 +1,13 @@
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item,index,array)=>{
+  // console.log(item,index,array)
+})
+
+const titulos = document.getElementsByClassName('titulo');
+
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach((item)=>{
+  // console.log(item)
+})
