b26fd625e13584659c39338eb977c7b390414a35
maicaosgz <malconaugusto@outlook.com>
2021-12-06 18:32:25 -0300

    foreach-e-arrowfunction (exercicio correção.) - dom-para-iniciantes

--- dom-para-iniciantes/foreach-e-arrowfunction/exercicio/index.js
+++ dom-para-iniciantes/foreach-e-arrowfunction/exercicio/index.js
@@ -1,26 +1,25 @@
 // Mostre no console cada parágrado do site
-  const cadaP  = document.querySelectorAll('p');
+const cadaP = document.querySelectorAll("p");
 
-  cadaP.forEach((p)=>{
-    console.log(p)
-  })
+cadaP.forEach((p) => {
+  console.log(p);
+});
 // Mostre o texto dos parágrafos no console
 
-cadaP.forEach((pText)=>{
+cadaP.forEach((pText) => {
   console.log(pText.innerText);
-})
+});
 
 // Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
+const imgs = document.querySelectorAll("img");
 
-imgs.forEach(item, index => {
+imgs.forEach((item, index) => {
   console.log(item, index);
 });
 
 let i = 0;
-imgs.forEach(()=> {
+imgs.forEach(() => {
   console.log(i++);
 });
 
-imgs.forEach(() => console.log(i++));
-
+imgs.forEach(() => i++);
