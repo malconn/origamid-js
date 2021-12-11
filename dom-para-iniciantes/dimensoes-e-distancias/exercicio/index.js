bc9815fcc6f78977439f80ae464e6db40c472129
maicaosgz <malconaugusto@outlook.com>
2021-12-11 18:13:01 -0300

    dimensoes-e-distancias (exercicio correcao.) - dom-para-iniciantes

--- dom-para-iniciantes/dimensoes-e-distancias/exercicio/index.js
+++ dom-para-iniciantes/dimensoes-e-distancias/exercicio/index.js
@@ -7,27 +7,32 @@ const topImg = topo.getBoundingClientRect().top;
 console.log(topImg);
 
 // Retorne a soma da largura de todas as imagens
-const todas = document.querySelectorAll("img");
+window.onload = () => {
+  const todas = document.querySelectorAll("img");
+  todas.forEach((img) => {
+    let widthImg = img.offsetWidth;
 
-todas.forEach((img) => {
-  let widthImg = img.getBoundingClientRect().width;
-  let heightImg = img.getBoundingClientRect().height;
+    let soma = 0;
 
-  let widthMax = (widthImg += widthImg);
-  let heightMax = (heightImg += heightImg);
+    soma += widthImg;
 
-  console.log(parseInt(widthMax + heightMax));
-});
+    console.log(parseInt(soma));
+  });
+};
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 const verifySize = document.querySelectorAll("a");
 
 verifySize.forEach((item) => {
-  const width = item.getBoundingClientRect().width;
-  const height = item.getBoundingClientRect().height;
-
-  console.log(item, width, height);
+  const width = item.offsetWidth;
+  const height = item.offsetHeight;
+  console.log(width, height);
+  if (width >= 48 && height >= 48) {
+    console.log(item, "possui acessibilidade");
+  } else {
+    console.log(item, "não possui boa acessibilidade");
+  }
 });
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
@@ -36,6 +41,4 @@ const menu = document.querySelector(".menu");
 
 if (window.matchMedia("(max-width:720px)").matches) {
   menu.classList.add("menu-mobile");
-} else {
-  menu.classList.remove("menu-mobile");
 }
