402bb9b4271710eaa505791bc9607a1e056890a9
maicaosgz <malconaugusto@outlook.com>
2021-12-16 00:01:39 -0300

    eventos (aula finalizada.) - dom-para-iniciantes

--- dom-para-iniciantes/eventos/aula/index.js
+++ dom-para-iniciantes/eventos/aula/index.js
@@ -1,56 +1,58 @@
-const img = document.querySelector('img');
+const img = document.querySelector("img");
 
-
-function callBack(event){
-  console.log(event)
+function callBack(event) {
+  console.log(event);
 }
-
 
 // img.addEventListener('click', callBack);
 
+const animaisLista = document.querySelector(".animais-lista");
 
-const animaisLista = document.querySelector('.animais-lista');
-
-function callBackLista(event){
+function callBackLista(event) {
   console.log(event.currentTarget);
   console.log(event.target);
   console.log(event.type);
 }
 
-
-animaisLista.addEventListener('click', callBackLista)
-
+// animaisLista.addEventListener("click", callBackLista);
 
 const linkExterno = document.querySelector('a[href^="http"]');
 
-function handleLinkExterno(event){
+function handleLinkExterno(event) {
   event.preventDefault();
 
-  console.log(event.currentTarget)
-  console.log(this)
+  console.log(event.currentTarget);
+  console.log(this);
 }
 
-linkExterno.addEventListener('click', handleLinkExterno);
+linkExterno.addEventListener("click", handleLinkExterno);
 
+const h1 = document.querySelector("h1");
 
-const h1 = document.querySelector('h1');
-
-function handleEvent(event){
-  console.log(event.type, event)
+function handleEvent(event) {
+  console.log(event.type, event);
 }
-
 
 // h1.addEventListener('click', handleEvent);
 // h1.addEventListener('mouseenter', handleEvent);
 // h1.addEventListener('mousemove', handleEvent);
 
-
 // window.addEventListener('scroll', handleEvent)
-function handleKeyboard(event){
-  if(event.key === 'f'){
-    document.body.classList.toggle('fullscreen-azul')
+function handleKeyboard(event) {
+  if (event.key === "f") {
+    document.body.classList.toggle("fullscreen-azul");
   }
-  console.log(event.key)
+  console.log(event.key);
 }
-window.addEventListener('keydown', handleKeyboard)
+window.addEventListener("keydown", handleKeyboard);
 
+const imgs = document.querySelectorAll("img");
+
+function handleImg(event) {
+  console.log(event.currentTarget.getAttribute("src"));
+}
+
+imgs.forEach((img) => {
+  console.log(img);
+  img.addEventListener("click", handleImg);
+});
