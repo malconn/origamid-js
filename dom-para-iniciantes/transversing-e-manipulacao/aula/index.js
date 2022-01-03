1cba26ef67462346cb89a10668901503f3e0dfb5
malconn <malconaugusto@outlook.com>
2022-01-03 15:17:30 -0300

    transversing-e-manipulacao (aula feita parte 2.) - dom-para-iniciantes

--- dom-para-iniciantes/transversing-e-manipulacao/aula/index.js
+++ dom-para-iniciantes/transversing-e-manipulacao/aula/index.js
@@ -1,11 +1,47 @@
-const h1 = document.querySelector("h1");
-const animaisLista = document.querySelector(".animais-descricao");
+// const h1 = document.querySelector("h1");
+// const animaisLista = document.querySelector(".animais-descricao");
 
-h1.innerHTML = "<p>Novo Título</p>";
+// h1.innerHTML = "<p>Novo Título</p>";
 
-const lista = document.querySelector(".animais-lista");
+// const lista = document.querySelector(".animais-lista");
 
-console.log(lista.children[--lista.children.length]);
-console.log(lista.querySelector("li:last-child"));
+// console.log(lista.children[--lista.children.length]);
+// console.log(lista.querySelector("li:last-child"));
 
-console.log(lista.previousElementSibling);
+// console.log(lista.previousElementSibling);
+
+// const animais = document.querySelector('.animais');
+
+// const contato = document.querySelector('.contato');
+
+// const titulo = contato.querySelector('.titulo');
+
+// const mapa = document.querySelector('.mapa');
+
+// contato.insertBefore(animais,mapa);
+
+
+// contato.replaceChild(mapa, titulo);
+
+
+
+// const novoh1 = document.createElement('h1');
+
+// novoh1.innerText="novo titulo";
+
+// novoh1.classList.add('titulo');
+
+// mapa.appendChild(novoh1)
+
+// console.log(novoh1)
+
+const h1 = document.querySelector('h1');
+
+const faq = document.querySelector('.faq');
+
+
+const cloneH1 = h1.cloneNode(true);
+
+cloneH1.classList.add('azul');
+
+faq.appendChild(cloneH1);
