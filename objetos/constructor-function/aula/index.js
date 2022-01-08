c58fdd1bc71f38517e37c43b9dc20d3ce9ae69fb
maicaosgz <malconaugusto@outlook.com>
2022-01-08 16:43:22 -0300

    constructor funcions (aula feita.) - objetos

---
+++ objetos/constructor-function/aula/index.js
@@ -0,0 +1,10 @@
+function Carro(marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro("Honda", 23000);
+
+const fiat = new Carro("Fiat", 30000);
+
+const chevrolet = new Carro("Chevrolet", 56000);
