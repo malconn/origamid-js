c3e13d337060cfdb9e9c0eff024300a1a4455a68
malconn <malconaugusto@outlook.com>
2022-01-10 16:26:51 -0300

    constructor function (exercicio correcao.) - objetos

--- objetos/constructor-function/exercicio/index.js
+++ objetos/constructor-function/exercicio/index.js
@@ -1,15 +1,24 @@
 // Transforme o objeto abaixo em uma Constructor Function
-function  pessoa (nome,idade) {
-  this.init = ()=> {
-    console.log(nome + ' - ' + idade + ' anos');
+// const pessoa = {
+//   nome: 'Nome pessoa',
+//   idade: 0,
+//   andar() {
+//     console.log(this.nome + ' andou');
+//   }
+// }
+
+function Pessoa (nome,idade){
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = ()=>{
+    console.log(this.nome + ' Andou');
   }
 }
-
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
-const joao = new pessoa('João', 20);
-const maria = new pessoa('Maria', 25);
-const bruno = new pessoa('Bruno', 15);
+const joao = new Pessoa('João', 20);
+const maria = new Pessoa('Maria', 25);
+const bruno = new Pessoa('Bruno', 15);
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -19,19 +28,22 @@ const bruno = new pessoa('Bruno', 15);
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
 
-function dom (seletor){
-  this.elements = document.querySelectorAll(seletor);
+function Dom (seletor){
+  const elementList = document.querySelectorAll(seletor);
+
+  this.elements = elementList;
 
   this.addClass = (classe)=>{
-    this.elements.forEach((item)=>{
+    elementList.forEach((item)=>{
       item.classList.add(classe);
     })
   }
 
   this.removeClass = (classe)=>{
-    this.elements.forEach((item)=>{
+    elementList.forEach((item)=>{
       item.classList.remove(classe);
     })
   }
+
 }
 
