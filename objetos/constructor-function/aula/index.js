4c08ed5c4a571fd2f7a5ca59eb6814bd7c5eb4d4
maicaosgz <malconaugusto@outlook.com>
2022-01-09 23:07:49 -0300

    constructor function (aula feita parte 2 - inicio) - objetos

--- objetos/constructor-function/aula/index.js
+++ objetos/constructor-function/aula/index.js
@@ -7,4 +7,11 @@ const honda = new Carro("Honda", 23000);
 
 const fiat = new Carro("Fiat", 30000);
 
-const chevrolet = new Carro("Chevrolet", 56000);
+function Carro2(marcaAtribuida, precoInicial) {
+  const taxa = 1.2;
+  const precoFinal = precoInicial * taxa;
+  this.marca = marcaAtribuida;
+  this.preco = precoFinal;
+}
+
+const mazda = new Carro2("Mazda", 5000);
