d8c841a1306a4534253cc35341db8395f115bc06
maicaosgz <malconaugusto@outlook.com>
2022-01-11 20:46:03 -0300

    prototype (exercicio ultima parte feita.) - objetos

--- objetos/prototype/exercicio/index.js
+++ objetos/prototype/exercicio/index.js
@@ -3,32 +3,32 @@
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
 
-function Pessoa(nome,sobrenome,idade){
+function Pessoa(nome, sobrenome, idade) {
   this.nome = nome;
   this.sobrenome = sobrenome;
   this.idade = idade;
 }
 
-Pessoa.prototype.nomeCompleto = function() {
-  return this.nome + ' ' +  this.sobrenome
-}
+Pessoa.prototype.nomeCompleto = function () {
+  return this.nome + " " + this.sobrenome;
+};
 
-const malcon = new Pessoa('Malcon', 'Augusto', 18)
+const malcon = new Pessoa("Malcon", "Augusto", 18);
 
-// Liste os métodos acessados por 
+// Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
 
 // Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
+const li = document.querySelector("li");
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+// li; => HTMLLIElement;
+// li.click; => Function;
+// li.innerText; => String;
+// li.value; => Number;
+// li.hidden; => Boolean;
+// li.offsetLeft; => Number;
+// li.click(); => não tem constructor do retorno undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+// li.hidden.constructor.name; => String;
