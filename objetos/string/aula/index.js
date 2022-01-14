296b7d7feb19a3f2f84b2130794b6cb4466a5c77
maicaosgz <malconaugusto@outlook.com>
2022-01-13 22:36:01 -0300

    string (aula segunda parte feita.) - objetos

--- objetos/string/aula/index.js
+++ objetos/string/aula/index.js
@@ -25,3 +25,29 @@ const listaPrecos = ["R$ 99", "R$ 199", "R$ 1200"];
 listaPrecos.forEach((item) => {
   console.log(item.padStart(15, "="));
 });
+
+const frase2 = "Ta";
+
+frase2.repeat(5);
+
+let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+// listaItens = listaItens.replace(/[ ]+/g, ", ");
+
+const arrayLista = listaItens.split(" ");
+
+console.log(arrayLista);
+
+const htmlText = "<div>O melhor item</div><div>A melhor lista</div>";
+const htmlArray = htmlText.split("div");
+const novoHtml = htmlArray.join("section");
+
+console.log(htmlText);
+console.log(htmlArray);
+
+const frutasArray = ["Banana", "Melancia", "Laranja"];
+
+const sexo1 = "Feminino";
+const sexo2 = "feminino";
+const sexo3 = "FEMININO";
+
+console.log(sexo1.toLowerCase() === "feminino");
