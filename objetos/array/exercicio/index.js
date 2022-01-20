2f948d799528a95bec5003643374fb468f2655b2
malconn <malconaugusto@outlook.com>
2022-01-20 15:10:34 -0300

    feat: array (exercicio feito.) - objetos

--- objetos/array/exercicio/index.js
+++ objetos/array/exercicio/index.js
@@ -1,77 +1,46 @@
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
+const primeiraComida = comidas.shift();
+const ultimaComida = comidas.pop();
 
-const transacoes = [
-  {
-    descricao: "Taxa do Pão",
-    valor: "R$ 39",
-  },
-  {
-    descricao: "Taxa do Mercado",
-    valor: "R$ 129",
-  },
-  {
-    descricao: "Recebimento de Cliente",
-    valor: "R$ 99",
-  },
-  {
-    descricao: "Taxa do Banco",
-    valor: "R$ 129",
-  },
-  {
-    descricao: "Recebimento de Cliente",
-    valor: "R$ 49",
-  },
-];
-let taxaTotal = 0;
-let recebimentoTotal = 0;
-transacoes.forEach((transacao) => {
-  const numeroLimpo = +transacao.valor.replace("R$", "");
+comidas.push('Arroz');
+comidas.unshift('Peixe','Batata');
 
-  if (transacao.descricao.slice(0, 4) === "Taxa") {
-    return (taxaTotal += numeroLimpo);
-  } else {
-    return (recebimentoTotal += numeroLimpo);
-  }
-});
-// Retorne uma array com a lista abaixo
-const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+console.log(comidas)
 
-const arrayTransportes = transportes.split(";");
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
+estudantes.sort()
+estudantes.reverse()
 
-// console.log(arrayTransportes);
+console.log(estudantes)
+console.log(estudantes.includes('Joana'))
+console.log(estudantes.includes('Juliana'))
 
-// Substitua todos os span's por a's
-let html = `<ul>
-                <li><span>Sobre</span></li>
-                <li><span>Produtos</span></li>
-                <li><span>Contato</span></li>
-              </ul>`;
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
 
-html = html.split("span").join("a").toLowerCase();
+html = html.split('section').join('ul');
+html = html.split('div').join('li');
 
-// console.log(html);
+console.log(html)
 
-// Retorne o último caracter da frase
-const frase = "Melhor do ano!";
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const cloneCarros = carros.slice();
+carros.pop();
 
-const finalCaractere = frase.slice(-1);
-
-// console.log(finalCaractere);
-// Retorne o total de taxas
-const transacoes2 = [
-  "Taxa do Banco",
-  "   TAXA DO PÃO",
-  "  taxa do mercado",
-  "depósito Bancário",
-  "TARIFA especial",
-];
-let taxasTotal = 0;
-transacoes2.forEach((transacao) => {
-  transacao = transacao.trimStart().toLowerCase().slice(0, 4);
-  if (transacao === "taxa") {
-    return taxasTotal++;
-  }
-});
-
-console.log(taxasTotal);
+console.log(carros)
+console.log(cloneCarros)
