728c8f1c94af37a3d3acf7b8ad02d7f4c8fc06fa
maicaosgz <malconaugusto@outlook.com>
2022-01-21 23:39:13 -0300

    feat: array-interacao (exercicio comecando a andar.) - objetos

--- objetos/array-interacao/exercicio/index.js
+++ objetos/array-interacao/exercicio/index.js
@@ -1,7 +1,22 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-
+const titulos = document.querySelectorAll(".curso h1");
+const descricao = document.querySelectorAll(".curso p");
+const aulas = document.querySelectorAll(".curso span.aulas");
+const horas = document.querySelectorAll(".curso span.horas");
+let cursos = [
+  {
+    title: titulos,
+    descricao: descricao,
+    aulas: aulas,
+    horas: horas,
+  },
+];
+const Aulas = cursos.reduce((acumulador, atual, index) => {
+  acumulador[index] = atual.aulas;
+  return acumulador;
+}, {});
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
