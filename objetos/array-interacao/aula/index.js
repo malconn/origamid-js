fcf5b8e08dd1df9ad8425c4090f717d441e1d5a9
maicaosgz <malconaugusto@outlook.com>
2022-01-21 22:32:34 -0300

    feat: array-interacao (segunda aula feita.) - objetos

--- objetos/array-interacao/aula/index.js
+++ objetos/array-interacao/aula/index.js
@@ -24,7 +24,6 @@
 // console.log(numeros)
 // console.log(numerosX2)
 
-
 // const aulas = [
 //   {
 //     nome: 'HTML 1',
@@ -53,24 +52,66 @@
 
 // console.log('Aulas',tempoAulas)
 
-
 // const aulas = [10,25,30];
 
 // const reduceAulas = aulas.reduce((acumulador,item)=>{
 //   console.log(`
-//   Acumulador => ${acumulador} 
-//   Item => ${item} 
+//   Acumulador => ${acumulador}
+//   Item => ${item}
 //   `)
 //   return item + acumulador
 // },0)
 
 // console.log(`Total do reduce => ${reduceAulas}`)
 
-const numeros = [10,25,30,2,3,54,33,22];
+// const numeros = [10,25,30,2,3,54,33,22];
 
+// const maiorNumero = numeros.reduce((anterior,atual)=>{
+//   return anterior > atual ? anterior : atual;
+// },0)
 
-const maiorNumero = numeros.reduce((anterior,atual)=>{
-  return anterior > atual ? anterior : atual;
-},0)
+// console.log(maiorNumero)
 
-console.log(maiorNumero)
+// const aulas = [
+//   {
+//     nome: "HTML 1",
+//     min: 15,
+//   },
+//   {
+//     nome: "HTML 2",
+//     min: 10,
+//   },
+//   {
+//     nome: "CSS 1",
+//     min: 20,
+//   },
+//   {
+//     nome: "JS 1",
+//     min: 25,
+//   },
+// ];
+
+// const listaAulas = aulas.reduce((acumulador, atual, index) => {
+//   acumulador[index] = atual.nome;
+//   return acumulador;
+// }, {});
+// console.log(listaAulas);
+const frutas = ["Banana", "Pêra", "Uva"];
+
+const temUva = frutas.some((item) => {
+  return item === "Uva";
+});
+
+const every = frutas.every((fruta) => {
+  return fruta;
+});
+
+console.log(every);
+
+const numeros = [6, 43, 22, 88, 101, 29];
+
+const maiorQue3 = numeros.every((n) => {
+  return n > 3;
+});
+
+console.log(maiorQue3);
