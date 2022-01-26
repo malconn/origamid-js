6722009edec78f304245831019505cd9cc052c4a
maicaosgz <malconaugusto@outlook.com>
2022-01-26 14:31:29 -0300

    feat: array-interacao (exercicio correcao feita.) - objetos

--- objetos/array-interacao/exercicio/index.js
+++ objetos/array-interacao/exercicio/index.js
@@ -1,29 +1,31 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-const titulos = document.querySelectorAll(".curso h1");
-const arrayTitulo = Array.from(titulos);
+const cursos = document.querySelectorAll(".curso");
+const arrayCurso = Array.from(cursos);
 
-const descricao = document.querySelectorAll(".curso p");
-const arrayDescricao = Array.from(descricao);
-
-const aulas = document.querySelectorAll(".curso span.aulas");
-const arrayAulas = Array.from(aulas);
-
-const horas = document.querySelectorAll(".curso span.horas");
-const arrayHoras = Array.from(horas);
-
-let cursos = [];
+const objetosCurso = arrayCurso.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aulas = curso.querySelector("span.aulas").innerText;
+  const horas = curso.querySelector("span.horas").innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  };
+});
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-const maiorNumero = numeros.reduce((anterior, atual) => {
-  return anterior < atual ? atual : anterior;
-});
+const maiorq100 = numeros.filter((numero) => numero > 100);
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
-const baixoTem = instrumentos.includes("Baixo");
+const baixoTem = instrumentos.some((instrumento) => {
+  return instrumento === "Baixo";
+});
 // Retorne o valor total das compras
 const compras = [
   {
@@ -47,12 +49,10 @@ const compras = [
     preco: "R$ 10,60",
   },
 ];
-const precos = compras.map((items) => {
-  return items.preco.slice(2).trim().replace(`,`, `.`);
-});
 
-let valorCompras = precos.reduce((acumulador, atual) => {
-  const total = +acumulador + +atual;
+let valorCompras = compras.reduce((acumulador, atual) => {
+  const precoLimpo = +atual.preco.slice(3).replace(",", ".");
+  const total = acumulador + precoLimpo;
   return total;
 }, 0);
 
