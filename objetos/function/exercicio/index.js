41e2c75ad52ca71b8b78dfa20331d5af88740018
malconn <malconaugusto@outlook.com>
2022-01-31 11:13:01 -0300

    feat: function (simplificando o queryselector) - objetos

--- objetos/function/exercicio/index.js
+++ objetos/function/exercicio/index.js
@@ -1,25 +1,22 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
-// const paragraphs = document.querySelectorAll("p");
-// const lengthParagraphs = Array.from(document.querySelectorAll("p")).reduce(
-//   (acumulador, atual) => {
-//     const length = acumulador + atual.innerText.length;
-//     return length;
-//   },
-//   0
-// );
+const paragraphs = document.querySelectorAll('p')
+const lengthParagraphs = Array.from(paragraphs).reduce((acumulador, atual) => {
+  const length = acumulador + atual.innerText.length
+  return length
+}, 0)
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
 function Dom(seletor) {
-  this.element = document.querySelector(seletor);
+  this.element = document.querySelector(seletor)
   this.classe = function (classe) {
-    this.element.classList.add(classe);
-  };
+    this.element.classList.add(classe)
+  }
   this.conteudo = function (conteudo) {
-    this.element.innerText = conteudo;
-  };
+    this.element.innerText = conteudo
+  }
 }
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
