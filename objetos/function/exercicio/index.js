a5993aab3e36e36437d5a425af9d6930d2bcab0f
malconn <malconaugusto@outlook.com>
2022-02-03 16:08:40 -0300

    feat: function (muadando algumas infos no exercicio.) - objetos

--- objetos/function/exercicio/index.js
+++ objetos/function/exercicio/index.js
@@ -1,25 +1,25 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
-const paragraphs = document.querySelectorAll("p");
+const paragraphs = document.querySelectorAll('p')
 const lengthParagraphs = Array.from(paragraphs).reduce((acumulador, atual) => {
-  const length = acumulador + atual.innerText.length;
-  return length;
-}, 0);
+  const length = acumulador + atual.innerText.length
+  return length
+}, 0)
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
 function Dom(seletor) {
-  this.element = document.querySelector(seletor);
+  this.elementTag = document.getElementsByTagName(seletor)
   this.classe = function (classe) {
-    this.element.classList.add(classe);
-  };
+    this.elementTag.classList.add(classe)
+  }
   this.conteudo = function (conteudo) {
-    this.element.innerText = conteudo;
-  };
+    this.elementTag.innerText = conteudo
+  }
 }
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
 
-const h1Novo = Dom.bind("h1", "titulo");
+const h1Novo = Dom.bind('h1', 'titulo')
