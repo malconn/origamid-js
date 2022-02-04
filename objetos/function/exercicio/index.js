21babf53b6fdf29c539f56982092fb52e9504d93
maicaosgz <malconaugusto@outlook.com>
2022-02-04 20:23:43 -0300

    test: function (other tentative before see the solution) - objetos

--- objetos/function/exercicio/index.js
+++ objetos/function/exercicio/index.js
@@ -1,25 +1,34 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
-const paragraphs = document.querySelectorAll('p')
+const paragraphs = document.querySelectorAll("p");
 const lengthParagraphs = Array.from(paragraphs).reduce((acumulador, atual) => {
-  const length = acumulador + atual.innerText.length
-  return length
-}, 0)
+  const length = acumulador + atual.innerText.length;
+  return length;
+}, 0);
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
-function Dom(seletor) {
-  this.elementTag = document.getElementsByTagName(seletor)
-  this.classe = function (classe) {
-    this.elementTag.classList.add(classe)
-  }
-  this.conteudo = function (conteudo) {
-    this.elementTag.innerText = conteudo
-  }
-}
+const Dom = {
+  elementTag: document.querySelector(this.elementTag),
+  classe: function (classe) {
+    return this.elementTag.classList.add(classe);
+  },
+  conteudo: function (conteudo) {
+    return (this.elementTag.innerText = conteudo);
+  },
+};
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
 
-const h1Novo = Dom.bind('h1', 'titulo')
+const h1Conteudo = {
+  texto: "OLÁ",
+  conteudo: function () {
+    return this.texto;
+  },
+};
+
+const h1NovoConteudo = h1Conteudo.conteudo;
+
+const h1Completo = h1NovoConteudo.bind(Dom, "h1", "titulo");
