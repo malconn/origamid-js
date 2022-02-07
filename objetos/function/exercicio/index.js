9b8ee100c0cea8191d6998968034e049b58eea6e
malconn <malconaugusto@outlook.com>
2022-02-07 17:02:26 -0300

    test: function (other tentative before see the solution) - objetos

--- objetos/function/exercicio/index.js
+++ objetos/function/exercicio/index.js
@@ -9,26 +9,15 @@ const lengthParagraphs = Array.from(paragraphs).reduce((acumulador, atual) => {
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
-const Dom = {
-  elementTag: document.querySelector(this.elementTag),
-  classe: function (classe) {
-    return this.elementTag.classList.add(classe);
-  },
-  conteudo: function (conteudo) {
-    return (this.elementTag.innerText = conteudo);
-  },
-};
+function DOM(element){
+  this.element = document.querySelector(element);
+  this.classe = (classe)=>{
+    return this.element.classList.add(classe);
+  };
+  this.conteudo = (conteudo)=>{
+    return this.element.innerText = conteudo
+  };
+}
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
-
-const h1Conteudo = {
-  texto: "OLÁ",
-  conteudo: function () {
-    return this.texto;
-  },
-};
-
-const h1NovoConteudo = h1Conteudo.conteudo;
-
-const h1Completo = h1NovoConteudo.bind(Dom, "h1", "titulo");
