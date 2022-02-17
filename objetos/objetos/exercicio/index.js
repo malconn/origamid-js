6e2a47b955c558b49f639f487b01ef51203676cb
maicaosgz <malconaugusto@outlook.com>
2022-02-16 22:25:44 -0300

    feat: objetos (correcao exercicio.) - objetos

--- objetos/objetos/exercicio/index.js
+++ objetos/objetos/exercicio/index.js
@@ -1,25 +1,30 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
-function verificarDado (dado){
+function verificarDado(dado) {
   return Object.prototype.toString.call(dado);
 }
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
-const quadrado = {
-  lados:4
-};
-Object.freeze(quadrado);
+const quadrado = {};
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+  },
+  // faces: {
+  //   value: 5,
+  // }, poderia sair criando essas propriedade e que não poderiam ser modificadas posteriormente
+});
 // Previna qualquer mudança
 // no objeto abaixo
 const configuracao = {
   width: 800,
   height: 600,
-  background: '#333'
-}
-Object.freeze(configuracao)
+  background: "#333",
+};
+Object.freeze(configuracao);
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
-const prototypeString =Object.getOwnPropertyNames(String.prototype)
-const prototypeArray = Object.getOwnPropertyNames(Array.prototype)
+const prototypeString = Object.getOwnPropertyNames(String.prototype);
+const prototypeArray = Object.getOwnPropertyNames(Array.prototype);
