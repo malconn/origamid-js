d7962017ac3422c6c0e18ec8f547283985a51129
malconn <malconaugusto@outlook.com>
2022-02-21 14:09:52 -0300

    feat: dataset (aula feita.) efeitos-no-dom

---
+++ efeitos-no-dom/dataset/aula/index.js
@@ -0,0 +1,9 @@
+let div = document.querySelector('[data-cor]');
+// console.log(Object.prototype.toString.call(div));
+div.dataset.height = 1000;
+
+delete div.dataset.width;
+
+div.dataset.totalHeight = 2000;
+
+console.log(div.dataset)
