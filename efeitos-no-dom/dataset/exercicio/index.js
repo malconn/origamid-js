0468f71ecd4794c4f4bb70e0b9b65f400a1e685f
malconn <malconaugusto@outlook.com>
2022-02-21 16:02:55 -0300

    feat: dataset (outra parte exercicio.) efeitos-no-dom

--- efeitos-no-dom/dataset/exercicio/index.js
+++ efeitos-no-dom/dataset/exercicio/index.js
@@ -2,6 +2,15 @@
 // data-anime="show-right" a todos as section's
 // com descricão dos animais.
 
+const dataShowDown = document.querySelectorAll('.animais-lista li');
+const dataShowRight = document.querySelectorAll('.animais-descricao section')
+dataShowDown.forEach((down)=>{
+  down.setAttribute('data-anime','show-down');
+})
+dataShowRight.forEach((right)=>{
+  right.setAttribute('data-anime','show-right');
+})
+
 // Utilizando estes atributos, adicione a classe
 // show-down ou show-right a sua respectiva section
 // assim que a mesma aparecer na tela (animacao tab)
@@ -13,5 +22,6 @@
 // Substitua todas as classes js- por data atributes.
 const substitute = document.querySelectorAll('[class*="js-"]');
 substitute.forEach((js)=>{
-  console.log(js.dataset.animate=js)
+  js.setAttribute('data-js',js.className)
+  console.log(js)
 })
